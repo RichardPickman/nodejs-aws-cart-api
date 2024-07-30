1887b0d1007224a1815e382b0d1e13dbd77d16f0
Richard Pickman <drezzerock@gmail.com>
2024-07-30 17:26:51 +0200

    update project initialization from default listening, to serverless

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,14 @@
-// import serverlessExpress from '@codegenie/serverless-express';
+import serverlessExpress from '@codegenie/serverless-express';
 import { NestFactory } from '@nestjs/core';
-// import { Callback, Context, Handler } from 'aws-lambda';
+import { Callback, Context, Handler } from 'aws-lambda';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 5000;
 
-// let server;
+let server;
 
-// async function bootstrap(): Promise<Handler> {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.enableCors({
-//     origin: (req, callback) => callback(null, true),
-//   });
-//   app.use(helmet());
-
-//   await app.init();
-
-//   const expressApp = app.getHttpAdapter().getInstance();
-
-//   return serverlessExpress({ app: expressApp });
-// }
-
-// export const handler: Handler = async (
-//   event: any,
-//   context: Context,
-//   callback: Callback,
-// ) => {
-//   server = server ?? (await bootstrap());
-
-//   console.log('App is running on %s port', port);
-
-//   return server(event, context, callback);
-// };
-
-async function bootstrap() {
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -43,9 +16,36 @@ async function bootstrap() {
   });
   app.use(helmet());
 
-  await app.listen(port);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  return serverlessExpress({ app: expressApp });
 }
 
-bootstrap().then(() => {
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+
   console.log('App is running on %s port', port);
-});
+
+  return server(event, context, callback);
+};
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: (req, callback) => callback(null, true),
+//   });
+//   app.use(helmet());
+
+//   await app.listen(port);
+// }
+
+// bootstrap().then(() => {
+//   console.log('App is running on %s port', port);
+// });
